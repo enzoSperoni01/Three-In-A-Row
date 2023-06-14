45a76ef5dfb659c03e28eae6984c6b388a3d8f4f
Enzo Speroni <speronienzo20@gmail.com>
2023-06-13 22:18:21 -0300

    Tomando turnos, alternando entre "X" y "O"

--- src/App.js
+++ src/App.js
@@ -9,12 +9,19 @@ function Square({ value, onSquareClick }) {
 };
 
 export default function Board() {
+  const [xIsNext, setXInNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+
+    if(xIsNext) nextSquares[i] = "X";
+    else nextSquares[i] = "O";
+
     setSquares(nextSquares);
+    setXInNext(!xIsNext);
   };
 
   return (
